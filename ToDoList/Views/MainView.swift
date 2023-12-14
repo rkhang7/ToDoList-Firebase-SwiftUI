76b7bc6bf41e140a9d93f55054b67f60d569af21
Khang <khang@tungvts-iMac-191.local>
2023-12-14 16:14:06 +0700

    Account UI

--- ToDoList/Views/MainView.swift
+++ ToDoList/Views/MainView.swift
@@ -11,10 +11,22 @@ struct MainView: View {
     @StateObject  var viewModel = MainViewViewModel()
     var body: some View {
         if viewModel.isSigned, !viewModel.currentUserId.isEmpty{
-            ToDoListView()
+            accountView
         }
         else{
             LoginView()
+        }
+    }
+    
+    @ViewBuilder
+    var accountView: some View{
+        TabView{
+            ToDoListView(userId: viewModel.currentUserId).tabItem{
+                Label("Home", systemImage: "house")
+            }
+            ProfileView().tabItem{
+                Label("Profile", systemImage: "person.circle")
+            }
         }
     }
 }
